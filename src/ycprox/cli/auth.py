5e2a51c20133bda529392c20f8ea58fe06817fec
Ilya Mogilin <mogiliniv@yandex-team.ru>
2025-12-07 19:55:19 +0400

    Adds a few parameters for Proxy class

--- src/ycprox/cli/auth.py
+++ src/ycprox/cli/auth.py
@@ -1,7 +1,9 @@
+from typing import Optional
 from pydantic_settings import CliSubCommand, CliApp
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from ycprox.core.secrets import vault
 from getpass import getpass
+from ycprox.core.config import settings
 
 oauth_url = "https://oauth.yandex.ru/authorize?response_type=token&client_id=1a6990aa636648e9b2ef855fa7bec2fb&_ym_uid=1737881378596538906&mc=v"
 
@@ -14,11 +16,15 @@ class Auth(BaseModel):
         print("Token saved successfully")
 
 class Proxy(BaseModel):
+    name: str = Field(description="Name of the proxy", default="proxy")
+    org_id: Optional[str] = Field(default=None, description="Organization ID")
+    cloud_id: Optional[str] = Field(default=None, description="Cloud ID")
+    folder_id: Optional[str] = Field(default=None, description="Folder ID")
 
     def cli_cmd(self) -> None:
         print("Proxy command")
 
-class Application(BaseModel):
+class Application(BaseModel, cli_prog_name=settings.cli_name):
     auth: CliSubCommand[Auth]
     proxy: CliSubCommand[Proxy]
 
